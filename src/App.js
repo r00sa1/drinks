64601161acde67594b5cb46349ba380691fb4bf0
Roosa <n2niro00@students.oamk.fi>
2023-10-03 12:19:32 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { FaHandHoldingHeart } from "react-icons/fa";
+
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
 function App() {
+  const [strDrink, setStrDrink] = useState(""); //nimi
+  const [strCategory, setStrCategory] = useState(""); //kategoria
+  const [strDrinkThumb, setDrinkThumb] = useState(""); //kuva
+  const [strAlcoholic, setStrAlcoholic] = useState("second");
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response.data);
+        setStrDrink(response.data.drinks[0].strDrink);
+        setStrCategory(response.data.drinks[0].strCategory);
+        setDrinkThumb(response.data.drinks[0].strDrinkThumb);
+        setStrAlcoholic(response.data.drinks[0].strAlcoholic);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        A drink just for You! <FaHandHoldingHeart />
+      </h3>
+      <p>
+        <b>Drink:</b> {strDrink}
+      </p>
+      <p>
+        <b>Category:</b> {strCategory}
+      </p>
+      <p>
+        <b>Alcoholic:</b> {strAlcoholic}
+      </p>
+      <img className="image" src={strDrinkThumb} alt={strDrink} />
     </div>
   );
 }
